2db88df3bad1ca59313f45c01fd8a55f9cb0ef50
93Contiue <scjy@liujiabin.local>
2016-03-04 11:53:04 +0800

    init

---
+++ MyFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirst
+//
+//  Created by 刘佳斌 on 16/3/4.
+//  Copyright © 2016年 Aries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirst
+//
+//  Created by 刘佳斌 on 16/3/4.
+//  Copyright © 2016年 Aries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
